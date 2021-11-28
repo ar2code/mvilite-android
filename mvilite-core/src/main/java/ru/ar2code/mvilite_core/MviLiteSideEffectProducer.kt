ed44fd08cb68746533e0324fdbc4fcdf2f8a7002
Alexey Rozhkov <alexey.rojkov@gmail.com>
2021-11-28 14:51:31 +0300

    refactoring

--- mvilite-core/src/main/java/ru/ar2code/mvilite_core/MviLiteSideEffectProducer.kt
+++ mvilite-core/src/main/java/ru/ar2code/mvilite_core/MviLiteSideEffectProducer.kt
@@ -8,12 +8,17 @@ interface MviLiteSideEffectProducer<M, S, E, R> {
     /**
      * Returns side effect depends on some logic and params.
      *
-     * @param state - current state of a ViewModel
-     * @param updatedState - if true reducer updated ViewModel's state
+     * @param currentState - current state of a ViewModel
+     * @param updatedState - if true reducer updated ViewModel's state before
      * @param message - dispatched message to ViewModel
      * @param result - some extra parameter (generally it is a result from interactor)
      *
      * @return side effect that will be emitted if not null.
      */
-    fun getEffectAfterStateUpdating(message: M, state: S, updatedState: Boolean, result: R?): E?
+    fun getEffectAfterStateUpdating(
+        message: M,
+        currentState: S,
+        updatedState: Boolean,
+        result: R?
+    ): E?
 }
